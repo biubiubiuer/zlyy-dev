bd18d5efbfcf535ace020c953824687d020ae848
biubiubiuer <ddarrylsluka4@gmail.com>
2022-09-28 00:41:51 +0800

    http to https

--- src/main/java/com/example/zlyy/ZlyyApplication.java
+++ src/main/java/com/example/zlyy/ZlyyApplication.java
@@ -1,10 +1,17 @@
 package com.example.zlyy;
 
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.example.zlyy.mapper")
@@ -13,5 +20,34 @@ public class ZlyyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZlyyApplication.class, args);
     }
+
+
+//    @Bean
+//    @ConditionalOnProperty(name = {"swsk33.server.enablehttps"}, havingValue = "true")
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector redirectConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(8803); // 原http端口
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443); // 跳转的https端口，也就是我们配置文件中配置的项目端口
+//        return connector;
+//    }
+
 
 }
--- src/main/java/com/example/zlyy/util/StringConstants.java
+++ src/main/java/com/example/zlyy/util/StringConstants.java
@@ -2,4 +2,7 @@ package com.example.zlyy.util;
 
 public class StringConstants {
     public static final String PMML_PATH = "/opt/zlyy/model/result.pmml";
+//    public static final String PMML_PATH = "C:\\zlyy\\model\\result.pmml";
+    
+    
 }
